5237529d2a477967e27d77f067257216b580dd04
Oak Supanat <oaksupanat@gmail.com>
2017-06-01 12:38:13 +0700

    change port to 5558

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var app = express();
-var port = 9999;
+var port = 5558;
 var cors = require('cors');
 //use ejs+express layouts
 app.set('view engine', 'ejs');
