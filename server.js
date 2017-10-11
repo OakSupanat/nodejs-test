f8cda9e232502d54188d18796a9ec11aeb0837e3
Oak Supanat <oaksupanat@gmail.com>
2017-10-12 00:42:25 +0700

    remove /search path

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded());
 app.use(cors());
 //route the app
 var router = require('./app/routes');
-app.use('/github-followers/*', router);
+app.use('/github-followers', router);
 app.use('/', router);
 
 
